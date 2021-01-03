1ddaf6482fedb573077ddd694357f8da6877af44
zerinoid <leo.zerino@gmail.com>
2021-01-03 06:46:06 -0300

    scripts adaptados para debian (nomes monitores)

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -15,11 +15,11 @@ echo "---" | tee -a /tmp/$bar1.log /tmp/$bar2.log
 #    MONITOR=$m polybar --reload $bar1 >>/tmp/$bar1.log 2>&1 &
 #done
 
-MONITOR="eDP1" polybar --reload $bar1 -c ~/.config/polybar/config.ini >>/tmp/$bar1.log 2>&1 &
+MONITOR="eDP-1" polybar --reload $bar1 -c ~/.config/polybar/config.ini >>/tmp/$bar1.log 2>&1 &
 
 externo=$(xrandr --query | grep "HDMI1")
 if [[ ! $externo = *disconnected* ]]; then
-  MONITOR="HDMI1" polybar --reload $bar2 -c ~/.config/polybar/config.ini >>/tmp/$bar2.log 2>&1 &
+  MONITOR="HDMI-1" polybar --reload $bar2 -c ~/.config/polybar/config.ini >>/tmp/$bar2.log 2>&1 &
 fi
 
 echo "Bars launched..."

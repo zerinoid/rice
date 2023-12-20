48b6fb661cc4535e8a9e275aca93826c2e1c4cba
zerinoid <leo.zerino@gmail.com>
2023-12-20 01:36:26 -0300

    restore old polybar

---
+++ .config/polybar/scripts/updates.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if ! updates=$(apt list --upgradable 2> /dev/null | grep -ivc listing); then
+    updates=0
+fi
+
+if [ "$updates" -gt 0 ]; then
+    echo "#$updates"
+else
+    echo ""
+fi

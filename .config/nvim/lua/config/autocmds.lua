698a28e7c73640c1f7c64f7ad769f4430e6bc82a
zerinoid <leo.zerino@gmail.com>
2024-03-07 15:45:31 -0300

    auto update lazyvim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -14,3 +14,17 @@ vim.api.nvim_command([[ autocmd FileType * setlocal formatoptions-=c formatoptio
 
 -- Relaunch Dunst
 -- vim.api.nvim_command([[ autocmd BufWritePost *dunstrc !pkill -SIGUSR1 -x dunst]])
+
+-- autoupdate
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = augroup("autoupdate"),
+  callback = function()
+    if require("lazy.status").has_updates then
+      require("lazy").update({ show = false })
+    end
+  end,
+})

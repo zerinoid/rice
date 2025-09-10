09df7ff6ebc84905f9740cb44d92149879d764d2
zerinoid <leo.zerino@gmail.com>
2025-09-10 20:20:51 -0300

    removed lazyvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "ntk148v/habamax.nvim", dependencies = { "rktjmp/lush.nvim" } },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "habamax",
-    },
-  },
-}
--- .config/nvim/lua/plugins/typst.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "kaarmu/typst.vim",
-  ft = "typst",
-  lazy = false,
-  keys = {
-    { "<localleader>ll", "<cmd>TypstWatch<cr>", desc = "Compile & Watch" },
-  },
-}

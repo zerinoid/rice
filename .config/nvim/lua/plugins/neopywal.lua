ecb8a3541af21e2b2ba567d61a4a431703f3c765
zerinoid <leo.zerino@gmail.com>
2026-07-25 18:44:14 -0300

    neopywal nvim and mimimeapps

---
+++ .config/nvim/lua/plugins/neopywal.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "RedsXDD/neopywal.nvim",
+    name = "neopywal",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme neopywal")
+    end,
+  },
+}
